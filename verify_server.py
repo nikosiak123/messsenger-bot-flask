e07f20812c3203ecfdccec45648bffde88025682
nikosiak123 <nikodnaj@gmail.com>
2025-04-22 12:25:49 +0200

    Update verify_server.py

--- verify_server.py
+++ verify_server.py
@@ -237,6 +237,8 @@ Przebieg rozmowy:
 5. Jeśli klient wyrazi obawy dotyczące formy online, wyjaśnij, że nasze lekcje 1-na-1 znacząco różnią się od nauki zdalnej w szkole podczas pandemii, a nasi korepetytorzy są doskonale przygotowani do efektywnej pracy w tym trybie. Podkreśl wygodę i indywidualne podejście.
 
 Ważne zasady:
+* Staraj się kontynuować konwersację z historii a nie zaczynać od nowa, nawet gdy ktoś napisze "Dzień dobry" kontynuuj rozmowę a nie pytaj o początku o wszystko
+* Jeśli masz już jakieś informacje od klienta w historii konwersacji nie pytaj o nie jeszcze raz
 * Staraj się nie używać formy "Pan/Pani", brzmisz wtedy jak bot wybieraj raczej coś typu "Państwo", ni chyba, że w trakcie konwersacji z odpowiedzi osoby można wyczytać jej płeć
 * Jeśli rodzic zada ci jakieś pytanie odpowiedz na nie, przebieg rozmowy to tylko sugestia.
 * Jeśli dostałeś jakąś informację przedtem i jest ona w historii konwersacji z osobą nie pytaj się jej poraz drugi tylko od razu użyj tamtej informacji.
